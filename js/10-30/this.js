153bd107d31f08c4eb043b67497423aa2b13fac2
ZWB <weirunsu@zai-art.com>
2020-11-05 18:48:50 +0800

    leetcode

--- js/10-30/this.js
+++ js/10-30/this.js
@@ -38,14 +38,36 @@
 // (a.c)();       //
 // (1, a.c)();    //
 
-const a = {
-  b: 66,
-  c: function (cb) {
-    setTimeout(() => {
-      return cb(this.b);
-    }, 500);
-  },
-};
-a.c(function (value) {
-  console.log(value); // 66
-});
+// const a = {
+//   b: 66,
+//   c: function (cb) {
+//     setTimeout(() => {
+//       return cb(this.b);
+//     }, 500);
+//   },
+// };
+// a.c(function (value) {
+//   console.log(value); // 66
+// });
+
+// var foo = { n: 1 };
+
+// (function(foo) {
+//   console.log(foo.n);
+//   foo.n = 3;
+//   foo = { n: 2 };
+//   console.log(foo.n);
+// })(foo)
+
+// console.log(foo.n);
+
+// console.log(Object.prototype.toString.call(1));
+
+
+function Person() {
+
+}
+
+var person = new Person();
+console.log(person.__proto__ == Person.prototype);
+console.log(Object.getPrototypeOf(person.__proto__));
