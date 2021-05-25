64e85556af45ef539788a30c34bcd02ca5c3750a
ZWB <weirunsu@zai-art.com>
2021-05-25 14:44:35 +0800

    手写系列更新

--- js/手写系列/promise/finally.js
+++ js/手写系列/promise/finally.js
@@ -1,3 +1,6 @@
+// finally 没有参数
+// finally 会将结果和 error 传递
+
 Promise.prototype._finally = function (cb) {
   return this.then(
     (value) => Promise.resolve(cb()).then(() => value),
@@ -9,12 +12,12 @@ Promise.prototype._finally = function (cb) {
 };
 
 Promise.resolve(1)
-  //   .then((res) => {
-  //     console.log("then", res);
-  //   })
-  //   .catch((err) => {
-  //     console.log("catch", err);
-  //   })
-  ._finally((res) => {
+  .then((res) => {
+    console.log("then", res);
+  })
+  .catch((err) => {
+    console.log("catch", err);
+  })
+  ._finally(() => {
     console.log("_finally", res);
   });
